c730d290a3061a775ec4153cdfd68938cd831443
WG-master <1441471911@qq.com>
2021-02-27 13:25:17 +0800

    设备部分开发基本完成

--- src/main/java/cn/pch/hospitaldevicesystem/controller/DeviceController.java
+++ src/main/java/cn/pch/hospitaldevicesystem/controller/DeviceController.java
@@ -2,14 +2,15 @@ package cn.pch.hospitaldevicesystem.controller;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.EnumUtil;
+import cn.pch.hospitaldevicesystem.entity.Device;
 import cn.pch.hospitaldevicesystem.enums.DeviceStateEnums;
 import cn.pch.hospitaldevicesystem.service.DeviceService;
+import cn.pch.hospitaldevicesystem.utils.MyDateUtils;
 import cn.pch.hospitaldevicesystem.utils.RestResponse;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.ClassUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -18,10 +19,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by 潘成花 on 2021/01/23
  */
+@Slf4j
 @RestController
 @RequestMapping("/device")
 public class DeviceController {
@@ -50,6 +53,44 @@ public class DeviceController {
         return RestResponse.ok(result);
     }
     /*
+    新增加设备
+    */
+    @PostMapping("/addDevice")
+    public RestResponse addDevice(@RequestBody Map<String,String> deviceInfo){
+        log.info("新增加的设备信息:{} ", JSON.toJSONString(deviceInfo));
+        Device device = new Device();
+        device.setHospitalId(Long.valueOf(deviceInfo.get("hospitalId")));
+        device.setAdminUserId(Long.valueOf(deviceInfo.get("adminUserId")));
+        device.setState(Integer.valueOf(deviceInfo.get("stateValue")));
+        device.setPictureUrl(deviceInfo.get("pictureUrl"));
+        device.setName(deviceInfo.get("name"));
+        device.setCreateName(deviceInfo.get("adminUserName"));
+        device.setCreateTime(MyDateUtils.GetNowDate());
+        deviceService.insertOneDevice(device);
+        return RestResponse.ok("新增成功");
+    }
+
+    /*
+    删除设备
+    */
+    @PostMapping("/deleteDevice")
+    public RestResponse deleteDevice(@RequestBody Map<String,String> deviceInfo){
+        log.info("删除设备主键id:{} ", JSON.toJSONString(deviceInfo));
+        deviceService.removeByid(Long.valueOf(deviceInfo.get("deviceId")));
+        return RestResponse.ok("删除成功");
+    }
+    /*
+    修改设备状态
+    */
+    @PostMapping("/updateDevice")
+    public RestResponse updateDevice(@RequestBody Map<String,String> deviceInfo){
+        log.info("修改设备主键id和将要修改的状态:{} ", JSON.toJSONString(deviceInfo));
+        Device updateDevice = deviceService.queryByid(Long.valueOf(deviceInfo.get("deviceId")));
+        updateDevice.setState(Integer.valueOf(deviceInfo.get("state")));
+        deviceService.insertOneDevice(updateDevice);
+        return RestResponse.ok("修改成功");
+    }
+    /*
         上传设备图片
     */
     @PostMapping("/upDevicePicture")
@@ -68,18 +109,19 @@ public class DeviceController {
         }
         //新的文件名以日期命名
         String fileName = DateUtil.format(new Date(), "yyyyMMddHHmmss")+"."+subffix;
-        System.out.println(fileName);
         //获取项目路径
         String path =  ClassUtils.getDefaultClassLoader().getResource("").getPath()+"static/images/devices";
+        //“windows下使用的是“\”作为分隔符,而linux则反其道而行之使用"/"作为分隔符。
         String realPath = path.replace('/', '\\').substring(1,path.length());
         File file = new File(realPath,fileName);
         try {
             uploadFile.transferTo(file);
+            System.out.println("图片上传成功 路径:");
         } catch (IOException e) {
             e.printStackTrace();
         }
         //用于查看路径是否正确
-        System.out.println(realPath);
+        System.out.println(realPath+"\\"+fileName);
         return RestResponse.ok("static/images/devices/"+fileName);
     }
 }
