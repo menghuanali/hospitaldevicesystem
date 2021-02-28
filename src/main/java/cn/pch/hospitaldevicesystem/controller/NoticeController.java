d64a6e8c82bd381172030583d8e17a2d7e3ce9e8
WG-master <1441471911@qq.com>
2021-02-28 12:32:19 +0800

    公告开发2

--- src/main/java/cn/pch/hospitaldevicesystem/controller/NoticeController.java
+++ src/main/java/cn/pch/hospitaldevicesystem/controller/NoticeController.java
@@ -1,10 +1,14 @@
 package cn.pch.hospitaldevicesystem.controller;
 
+import cn.hutool.core.util.EnumUtil;
+import cn.pch.hospitaldevicesystem.enums.NoticeTypeEnums;
 import cn.pch.hospitaldevicesystem.service.NoticeService;
 import cn.pch.hospitaldevicesystem.utils.RestResponse;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 潘成花 on 2021/01/23
@@ -30,5 +34,17 @@ public class NoticeController {
     @GetMapping("/getOneNotice")
     public RestResponse getOneNotice(@RequestParam(name = "id") String id){
         return RestResponse.ok(noticeService.queryByid(Long.valueOf(id)));
+    }
+    /*
+    得到公告类型枚举
+    */
+    @PostMapping("/getTypeEnums")
+    public RestResponse getTypeEnums(){
+        List<Object> names = EnumUtil.getFieldValues(NoticeTypeEnums.class, "name");
+        List<Object> types = EnumUtil.getFieldValues(NoticeTypeEnums.class, "code");
+        List<Object> result = new ArrayList<>();
+        result.add(names);
+        result.add(types);
+        return RestResponse.ok(result);
     }
 }
