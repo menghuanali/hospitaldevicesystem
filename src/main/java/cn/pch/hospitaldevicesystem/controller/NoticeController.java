2b4b790bf820c873327bbdf5d9fb570a37606072
WG-master <1441471911@qq.com>
2021-03-06 19:21:04 +0800

    公告开发

--- src/main/java/cn/pch/hospitaldevicesystem/controller/NoticeController.java
+++ src/main/java/cn/pch/hospitaldevicesystem/controller/NoticeController.java
@@ -1,5 +1,6 @@
 package cn.pch.hospitaldevicesystem.controller;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.EnumUtil;
 import cn.pch.hospitaldevicesystem.entity.Notice;
 import cn.pch.hospitaldevicesystem.enums.NoticeTypeEnums;
@@ -7,10 +8,15 @@ import cn.pch.hospitaldevicesystem.service.NoticeService;
 import cn.pch.hospitaldevicesystem.utils.MyDateUtils;
 import cn.pch.hospitaldevicesystem.utils.RestResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -67,5 +73,40 @@ public class NoticeController {
         notice.setCreateTime(MyDateUtils.GetNowDate());
         noticeService.insertOneNotice(notice);
         return RestResponse.ok("发布成功");
+    }
+
+    /*
+    上传公告图片
+    */
+    @PostMapping("/upNoticePicture")
+    public RestResponse uploadDevicePicture(@RequestParam("noticePicture") MultipartFile uploadFile){
+        if (uploadFile.isEmpty()) {
+            return RestResponse.fail("上传文件不能为空");
+        }
+        //获取原文件名
+        String name=uploadFile.getOriginalFilename();
+        //获取文件后缀
+        String subffix=name.substring(name.lastIndexOf(".")+1,name.length());
+        //控制格式
+        if(subffix.equals("")&&!subffix.equals("jpg")&&!subffix.equals("jpeg")&&!subffix.equals("png"))
+        {
+            return RestResponse.fail("上传文件格式不对");
+        }
+        //新的文件名以日期命名
+        String fileName = DateUtil.format(new Date(), "yyyyMMddHHmmss")+"."+subffix;
+        //获取项目路径
+        String path =  ClassUtils.getDefaultClassLoader().getResource("").getPath()+"static/images/notice";
+        //“windows下使用的是“\”作为分隔符,而linux则反其道而行之使用"/"作为分隔符。
+        String realPath = path.replace('/', '\\').substring(1,path.length());
+        File file = new File(realPath,fileName);
+        try {
+            uploadFile.transferTo(file);
+            System.out.println("图片上传成功 路径:");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //用于查看路径是否正确
+        System.out.println(realPath+"\\"+fileName);
+        return RestResponse.ok("static/images/notice/"+fileName);
     }
 }
